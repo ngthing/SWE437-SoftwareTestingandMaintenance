7e21d68fa16357ee49f4b98e1784436d294988d8
Kien Nguyen <knguye55@gmu.edu>
2017-02-15 23:15:46 -0500

    Update AbstractListEqualsMethodTest.java

--- HW#3/src/AbstractListEqualsMethodTest.java
+++ HW#3/src/AbstractListEqualsMethodTest.java
@@ -9,4 +9,23 @@ public class AbstractListEqualsMethodTest {
 		fail("Not yet implemented");
 	}
 
+	@Test public void testIfBranch1(){
+		ArrayList<String> alist1 = new ArrayList<String>();
+		alist1.add("this");
+		alist1.add("is");
+		alist1.add("first");
+		alist1.add("if");
+		alist1.add("test");
+		assertTrue(alist1.equals(alist1));
+	}
+	@Test public void testIfBranch1(){
+		ArrayList<String> alist1 = new ArrayList<String>();
+		alist1.add("this");
+		alist1.add("is");
+		alist1.add("first");
+		alist1.add("if");
+		alist1.add("test");
+		assertTrue(alist1.equals(alist1));
+	}
+	
 }
